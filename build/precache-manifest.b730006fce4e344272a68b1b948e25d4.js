850d1029d2005ea1a31d9bd30e4f2f69d1f57aa7
Robert Rossman <brossman01@outlook.com>
2021-07-31 09:47:14 -0400

    add origin

---
+++ build/precache-manifest.b730006fce4e344272a68b1b948e25d4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4bfa1aaa090b39af43ef2b1816d8f5",
+    "url": "/PortfolioReact/index.html"
+  },
+  {
+    "revision": "d59b31d7afbe9d265036",
+    "url": "/PortfolioReact/static/css/main.f5bbb9fd.chunk.css"
+  },
+  {
+    "revision": "e8510e50cd7d65205673",
+    "url": "/PortfolioReact/static/js/2.1d2e86b7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/PortfolioReact/static/js/2.1d2e86b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d59b31d7afbe9d265036",
+    "url": "/PortfolioReact/static/js/main.b985ad43.chunk.js"
+  },
+  {
+    "revision": "f36ee00b6144b0467943",
+    "url": "/PortfolioReact/static/js/runtime-main.b40e3afe.js"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/PortfolioReact/static/media/02-hero-bg.402a471b.jpg"
+  },
+  {
+    "revision": "744105778413b0bbf30fc022b102d023",
+    "url": "/PortfolioReact/static/media/Budget.74410577.png"
+  },
+  {
+    "revision": "069d5be348ae3fc69dd3457c897fea9e",
+    "url": "/PortfolioReact/static/media/Pokemon2.069d5be3.png"
+  },
+  {
+    "revision": "16f43f81a0616b39087000e63f4a1784",
+    "url": "/PortfolioReact/static/media/Robert_Rossman_Resume.16f43f81.PDF"
+  },
+  {
+    "revision": "d993b41c719101f018761741c80e8760",
+    "url": "/PortfolioReact/static/media/TeamProfile.d993b41c.png"
+  },
+  {
+    "revision": "fbe4471474b0fe49d8341957e75830c1",
+    "url": "/PortfolioReact/static/media/dayplanner.fbe44714.jpg"
+  },
+  {
+    "revision": "e14f57d581f975b894fabfb208619110",
+    "url": "/PortfolioReact/static/media/picture of me.e14f57d5.jpg"
+  },
+  {
+    "revision": "98d561d3b7929e064fe4f37e632e2ccc",
+    "url": "/PortfolioReact/static/media/searchNews.98d561d3.png"
+  },
+  {
+    "revision": "6c4c82c78ec45c751de936616ff63c2c",
+    "url": "/PortfolioReact/static/media/weatherdashboard2.6c4c82c7.png"
+  }
+]);
